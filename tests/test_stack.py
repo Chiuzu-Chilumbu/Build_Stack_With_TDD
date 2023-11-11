b28fd938636fc724a2dc46cd1309dd043ac3b3b5
Chiuzu-chilumbu <chiuzubenny@hotmail.com>
2023-11-11 12:24:54 +0800

    tests have been rafactored

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -8,8 +8,4 @@ from stack_adt import Stack
 def test_should_create_stack_object_from_stack_class():
 	stack_object = Stack()
 	assert stack_object is not None
-
-
-def test_should_have_stack_object_that_is_instance_of_stack_class():
-	stack_object = Stack()
 	assert isinstance(stack_object, Stack)
